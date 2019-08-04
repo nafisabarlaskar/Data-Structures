7caa8a6e92bdd169b52bb6ed725b13d2830d9133
nafisabarlaskar <nafisa.barlaskar@gmail.com>
2019-08-04 18:00:09 +0530

    Some more methods on heap

--- DataStructures/src/Heap/Heap.java
+++ DataStructures/src/Heap/Heap.java
@@ -23,6 +23,22 @@ public class Heap {
             this.count = count;
         }
     }
+
+    public class Tuple implements Comparable<Tuple>{
+        int x;
+        int y;
+        int val;
+        public Tuple(int x, int y, int val){
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Tuple o) {
+            return this.val - o.val;
+        }
+    }
     public List<Integer> getTopKFrequentElements(int[] arr, int k){
 
         PriorityQueue<Pair> priorityQueue = new PriorityQueue<>(Comparator.comparing(Pair::getCount));
@@ -47,5 +63,23 @@ public class Heap {
             result.add(p.getVal());
         }
         return result;
+    }
+
+    public int kthSmallestInMatrix(int[][] matrix, int k){
+        if(matrix.length == 0){
+            return 0;
+        }
+        PriorityQueue<Tuple> priorityQueue = new PriorityQueue<>();
+        for(int i = 0; i < matrix.length - 1; i++){
+            priorityQueue.add(new Tuple(i, 0, matrix[i][0]));
+        }
+        for(int i = 0; i < k-1; i++){
+            Tuple tuple = priorityQueue.poll();
+            if(tuple.x == matrix.length-1){
+                continue;
+            }
+            priorityQueue.add(new Tuple(tuple.x+1, tuple.y, matrix[tuple.x+1][tuple.y]));
+        }
+        return priorityQueue.peek().val;
     }
 }
