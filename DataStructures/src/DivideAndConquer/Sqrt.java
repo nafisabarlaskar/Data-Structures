209058461a6d415413205cb76f9a6f2812bce48b
nafisabarlaskar <nafisa.barlaskar@gmail.com>
2019-08-07 19:07:10 +0530

    Square root of a number using divide and conquer

---
+++ DataStructures/src/DivideAndConquer/Sqrt.java
@@ -0,0 +1,24 @@
+package DivideAndConquer;
+
+public class Sqrt {
+
+    public static int sqrt(int x){
+        if (x == 0)
+            return 0;
+        int left = 1, right = Integer.MAX_VALUE;
+        while (true) {
+            int mid = left + (right - left)/2;
+            if (mid > x/mid) {
+                right = mid - 1;
+            } else {
+                if (mid + 1 > x/(mid + 1))
+                    return mid;
+                left = mid + 1;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(sqrt(9));
+    }
+}
