134085bcad8c7d536c801b8400b569bbdadb6a91
nafisabarlaskar <nafisa.barlaskar@gmail.com>
2019-08-13 09:05:34 +0530

    Number of Rescue Boats = Leetcode

---
+++ DataStructures/src/TwoPointer/NumberOfRescueBoats.java
@@ -0,0 +1,15 @@
+package TwoPointer;
+
+import java.util.Arrays;
+
+public class NumberOfRescueBoats {
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int ans = 0;
+        for (int hi = people.length - 1, lo = 0; hi >= lo; --hi, ++ans) {
+            if (people[lo] + people[hi] <= limit) { ++lo; }
+        }
+        return ans;
+    }
+}
